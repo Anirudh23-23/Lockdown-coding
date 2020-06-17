f51ca594e3061d1943d80ca2580babd52315cbb3
Anirudh23-23 <53473025+Anirudh23-23@users.noreply.github.com>
2020-06-17 20:53:20 +0530

    Create Count(not 3).c

---
+++ Count(not 3).c
@@ -0,0 +1,40 @@
+int count(int n) 
+{ 
+
+    if (n < 3) 
+
+        return n; 
+
+    if (n >= 3 && n < 10) 
+
+       return n-1;
+
+    int power = 1; 
+
+    while (n/power > 9) 
+
+        power = power*10; 
+
+    int x=n/power; 
+
+  
+
+    if (x!=3) 
+
+
+      return count(x)*count(power - 1) + count(x) + count(n%power); 
+
+    else
+
+
+      return count(x*power - 1); 
+} 
+
+
+int main() 
+{ 
+
+    printf ("%d ", count(578)); 
+
+    return 0; 
+} 
